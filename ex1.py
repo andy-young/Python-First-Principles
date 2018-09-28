20e63970d3a38aa29c2526161cd415cb05ca4d9c
Andy Young <ayscript@gmail.com>
2018-09-28 11:40:31 -0600

    complete exercise 1

---
+++ ex1.py
@@ -0,0 +1,12 @@
+print("Hello World")
+print("Hello Again")
+print("Hello BootCamper's Collective!")
+print("My name is Andy.")
+print("'print()' is a python method.")
+print("Python Docs are here --> https://docs.python.org/3/")
+print("This line has nested 'single' quotes.")
+print('This line has nested "double quotes".')
+print('Now toggle into your terminal and type: python3 ex1.py')
+print('This symbol --> # <-- is called an "octothorpe", a "mesh", "pound", or "hash".')
+# print("Remove the hash from the beginning of this line, so the python interpreter can print this text.")
+# <-- this hash tell python to comment.
