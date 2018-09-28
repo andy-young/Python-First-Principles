e6860742580a50b0bad1e7b1b2b7f024c55cb736
Andy Young <ayscript@gmail.com>
2018-09-28 12:49:16 -0600

    complete lessons 1 through 3

---
+++ Numbers_and_Math/ex3.py
@@ -0,0 +1,39 @@
+# +: plus
+# -: minus
+# /: slash
+# *: asterisk
+# %: percent
+# <: less-than
+# >: greater-than
+# <=: less-than-equal
+# >=: greater-than-equal
+
+print("I will now count my chickens:")
+
+print("Hens", 25 + 30 / 6)
+print("Roosters", 100 - 25 * 3 % 4)
+
+print("Now I will count the eggs:")
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print("Is it true that 3 + 2 < 5 - 7?")
+
+print(3 + 2 < 5 - 7)
+
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+print("Oh, that's why it's false.")
+
+print("How about some more.")
+
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
+
+# Study Drills
+# 1. Comment above each line and explain what's happening below it. Notice that print() takes arguments..
+# 2. Open the Python Interpreter and use it like a calculator
+# 3. Rewrite this program with 'floating point' numbers for more accuracy --> 20 becomes 20.0
+# 4. Look up the 'modulo' operator in the Python Docs --> https://docs.python.org/3/reference/expressions.html
