a3788c7c64aea5f097fa3f38810aabe262ccf893
Andy Young <ayscript@gmail.com>
2018-10-02 13:16:22 -0600

    complete exercise 11 & 12

---
+++ ex12_Prompting_People/ex12.py
@@ -0,0 +1,9 @@
+# For input, you can also put in a prompt to show to a person
+# so she knows what to type...
+# Now we can completely rewrite our previous exercise...
+
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weight? ")
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
