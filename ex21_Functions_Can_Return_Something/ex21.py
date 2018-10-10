9d7d184e2acb52e9bbd10992df9f6676e49f29d7
Andy Young <ayscript@gmail.com>
2018-10-10 14:12:41 -0600

    complete exercise 21

---
+++ ex21_Functions_Can_Return_Something/ex21.py
@@ -0,0 +1,33 @@
+def add(a, b):
+    print(f"ADDING {a} + {b}")
+    return a + b
+
+def subtract(a, b):
+    print(f"SUBTRACTING {a} - {b}")
+    return a - b
+
+def multiply(a, b):
+    print(f"MULTIPLYING {a} * {b}")
+    return a * b
+
+def divide(a, b):
+    print(f"DIVIDING {a} / {b}")
+    return a / b
+
+
+print("Let's do some math with just functions!")
+
+age = add(30, 8)
+height = subtract(70, 4)
+weight = multiply(50, 3)
+iq = divide(100, 99)
+
+print(f"Age: {age}, Height: {height}, Weight: {weight}, IQ: {iq}")
+
+
+# A puzzle for the extra credit, type it in anyway.
+print("Here is a puzzle.")
+
+what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+
+print("That becomes: ", what, "Can you do it by hand?")
