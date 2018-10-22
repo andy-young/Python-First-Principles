d92be2579457e8dd84edd8e776a2aabed16d06aa
Andy Young <ayscript@gmail.com>
2018-10-22 01:16:41 -0600

    Complete ex26.

--- Python-Lessons/ex25_Even_More_Practice/ex25.py
+++ Python-Lessons/ex25_Even_More_Practice/ex25.py
@@ -2,12 +2,11 @@ def break_words(stuff):
     """This function will break up words for us."""
     words = stuff.split(' ')
     return words
-
+    
 
 def sort_words(words):
     """Sorts the words."""
     return sorted(words)
-
 
 def print_first_word(words):
     """Prints the first word after popping it off."""
@@ -19,7 +18,6 @@ def print_last_word(words):
     """Print the last word after popping it off."""
     word = words.pop(-1)
     print(word)
-
 
 def sort_sentence(sentence):
     """Takes in a full sentence and returns the sorted words."""
