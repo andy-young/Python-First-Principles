c1b821aac36e47ee1acb722bd40de540286438a3
Andy Young <ayscript@gmail.com>
2018-11-01 13:26:18 -0600

    Complete ex35.

--- Python-Lessons/ex35_Branches_and_Functions/ex35.py
+++ Python-Lessons/ex35_Branches_and_Functions/ex35.py
@@ -1,18 +1,85 @@
-animals = ['bear', 'python3.6', 'peacock', 'kangaroo', 'whale', 'platypus']
+from sys import exit
 
-print(animals)
+def gold_room():
+    print("This room is full of gold, How much do you take?")
 
-print(f"The animal at 1 is the 2nd animal & is a '{animals[1]}'.")
+    choice = input('> ')
+    if "0" in choice or "1" in choice:
+        how_much = int(choice)
 
-print(f"The 3rd animal is '{animals[2]}' and is at 2.")
+    else:
+        dead("Man, learn to type a number.")
 
-print(f"The animal at 3 is the 4th animal & is a '{animals[3]}'.")
+    if how_much < 50:
+        print("Nice, you're not greedy, you win!")
+        exit(0)
 
-print(f"The 5th animal is '{animals[4]}' & is at 4.")
+    else:
+      dead("You greedy bastard!")
 
-print(f"The animal at 2 is '{animals[2]}' & is the 3rd animal.")
 
-print(f"The 6th animal is '{animals[5]}' & is at 5.")
+def bear_room():
+    print("There is a bear here.")
+    print("The bear has a bunch of honey.")
+    print("The fat bear is in front of another door.")
+    print("How are you going to move the bear?")
+    bear_moved = False
 
-print(f"The animal at 4 is the 5th animal & is a '{animals[4]}'.")
+    while True:
+        choice = input("> ")
 
+        if choice == 'take honey':
+            dead("The bear looks at you then slaps your face off.")
+
+        elif choice == "taunt bear" and not bear_moved:
+            print("The bear has moved from the door.")
+            print("You can 'open the door' now.")
+            bear_moved = True
+
+        elif choice == "taunt bear" and bear_moved:
+            dead("The bear gets pissed off and chews your leg off.")
+
+        elif choice == "open the door" and bear_moved:
+            gold_room()
+
+        else:
+            print("I go no idea what that means.")
+
+def cthulhu_room():
+    print("Here you see the great evil Cthulhu.")
+    print("He, it, whatever stares at you and you go insane.")
+    print("Do you 'flee' for your life or eat your 'head'?")
+
+    choice = input("> ")
+
+    if "flee" in choice:
+        start()
+
+    elif "head" in choice:
+        dead("Well that was tasty.")
+
+    else:
+        cthulhu_room()
+
+
+def dead(why):
+    print(why, "Good job!")
+    exit(0)
+
+def start():
+    print("You are in a dark room.")
+    print("There is a door to your 'right' and 'left'.")
+    print("Which one do you take?")
+
+    choice = input("> ")
+
+    if choice == "left":
+        bear_room()
+
+    elif choice == "right":
+        cthulhu_room()
+
+    else:
+        dead("You stumble around the room until you starve.")
+
+start()
