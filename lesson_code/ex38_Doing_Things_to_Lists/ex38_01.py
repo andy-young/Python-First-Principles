b8dab5ae0778cc13d4aee6a198d851bea99697fd
Andy Young <ayscript@gmail.com>
2018-11-08 13:49:12 -0700

    Practice ex38

---
+++ lesson_code/ex38_Doing_Things_to_Lists/ex38_01.py
@@ -0,0 +1,15 @@
+# Create a space separated string of 2 words called 'six_things'
+
+six_things = "Computer Capsule"
+
+objects = six_things.split(None) # ["Computer", "Capsule"]
+print("'six_things' was split and assigned to 'objects'.")
+
+print(f"\tObjects -> {objects}")
+
+desk_items = ["Mouse", "Keyboard", "Cords", "Lamp", "Mic", "Camera"]
+
+while len(objects) != 8:
+    
+
+
