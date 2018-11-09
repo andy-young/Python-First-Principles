d3d38c6b867eee6b35efe09a8dd21456244c57d5
Andy Young <ayscript@gmail.com>
2018-11-09 09:57:22 -0700

    Practice ex38 some more

--- lesson_code/ex38_Doing_Things_to_Lists/ex38_01.py
+++ lesson_code/ex38_Doing_Things_to_Lists/ex38_01.py
@@ -3,13 +3,25 @@
 six_things = "Computer Capsule"
 
 objects = six_things.split(None) # ["Computer", "Capsule"]
-print("'six_things' was split and assigned to 'objects'.")
+print("'six_things' was split and assigned to 'objects'.\n")
 
-print(f"\tObjects -> {objects}")
+print(f"\tObjects -> {objects}\n")
 
 desk_items = ["Mouse", "Keyboard", "Cords", "Lamp", "Mic", "Camera"]
 
 while len(objects) != 8:
-    
+    a_desk_item = desk_items.pop()
+    print(f"Appending: {a_desk_item} to {objects}\n")
+    objects.append(a_desk_item)
+    print(f"There are {len(objects)} objects now.\n")
+    print(f"And 'desk_items' are being removed! -> {desk_items}\n")
 
 
+print(f"Let's see objects increasing: {objects}\n")
+
+print("Let's take some objects.\n")
+
+print(f"The second item is the list is {objects[1]}.\n")
+print(f"The second to last item is {objects[-2]}\n")
+print(f"The last object is {objects.pop()}.\n")
+print(f"Smileys in between objects -> {'--'.join(objects[2:6])}\n")
