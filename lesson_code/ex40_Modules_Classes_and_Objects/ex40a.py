d8e5dd818fc3e6199368995479883bb5ee315bd9
Andy Young <ayscript@gmail.com>
2018-11-12 14:40:38 -0700

    Complete ex40

---
+++ lesson_code/ex40_Modules_Classes_and_Objects/ex40a.py
@@ -0,0 +1,10 @@
+import mystuff
+
+mystuff.apple()
+
+print(mystuff.tangerine)
+
+# A class, in python, is a way to take a grouping
+# of functions or data and place them inside a
+# container so you can access them with the . (dot)
+# operator.
