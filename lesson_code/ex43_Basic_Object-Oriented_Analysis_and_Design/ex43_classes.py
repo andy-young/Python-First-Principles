f8d30fbeb26b7787b29f9b7fe6ccba1b60e8454f
Andy Young <ayscript@gmail.com>
2018-11-20 14:18:17 -0700

    work on ex43

---
+++ lesson_code/ex43_Basic_Object-Oriented_Analysis_and_Design/ex43_classes.py
@@ -0,0 +1,49 @@
+class Scene(object):
+
+    def enter(self):
+        pass
+
+
+class Engine(object):
+
+    def __init__(self, scene_map):
+        pass
+
+    def play(self):
+        pass
+
+
+class Death(Scene):
+
+    def enter(self):
+        pass
+
+
+class CentralCorridor(Scene):
+
+    def enter(self):
+        pass
+
+
+class LaserWeaponArmory(Scene):
+
+    def enter(self):
+        pass
+
+
+class TheBridge(Scene):
+
+    def enter(self):
+        pass
+
+
+class EscapePod(Scene):
+
+    def enter(self):
+        pass
+
+
+class Map(object):
+
+    def __init__(self, start_scene):
+      pass
